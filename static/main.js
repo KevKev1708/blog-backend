b416e75a602f798dbb9f9a31748aa78fecda8b5f
KevKev1708 <weisekevin@gmx.de>
2019-10-30 14:14:54 +0100

    delete static folder

--- static/main.js
+++ static/main.js
@@ -7,18 +7,26 @@ fetch('http://localhost:8080/blogs')
 //die Daten rein
 function buildPosts(posts) {
     posts.forEach(post => {
-        let div = document.createElement("div");
-        div.classList.add("post");
-        let h2 = document.createElement("h2");
-        h2.innerHTML = post.title;
-        div.appendChild(h2);
+        let div = document.createElement("li");
+        let h6 = document.createElement("h6");
+        h6.classList.add("collapsible-header");
+        h6.innerHTML = post.title;
+        div.appendChild(h6);
+        let icon = document.createElement("i");
+        icon.classList.add("material-icons");
+        icon.innerHTML = "textsms";
+        h6.prepend(icon);
         let p = document.createElement("p");
+        p.classList.add("collapsible-body");
         p.innerHTML = post.body;
         div.appendChild(p);
         //hier wird der erstellte div-Bereich an den schon bestehenden div-Bereich angeschoben
         document.getElementById('posts').prepend(div)
-
-        console.log(div);
-        console.log(p);
     })
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+    let elems = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(elems);
+
+});
